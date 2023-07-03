b917dbddd657d69ebb3f807079629782376cf4b0
Loknath Mishra <loknath.mishra@framsikt.no>
2023-07-03 11:23:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'vowel-counter-app';
+  countingVowel = true;
+  count=0;
+  text:string='';
+  handleChange(){
+    this.count=0;
+    if (this.countingVowel){ for (let i = 0; i < this.text.length; i++) {
+      if(this.isVowel(this.text[i]))
+      {
+        this.count = this.count+1;
+      }
+    }}
+    else
+    {
+      for (let i = 0; i < this.text.length; i++) {
+        if(!this.isVowel(this.text[i]) && this.isAlpha(this.text[i]))
+        {
+          this.count = this.count+1;
+        }
+      }
+    }
+  }
+
+  resetText(){
+    this.text="";
+    this.handleChange();
+  }
+
+  toggleCountType(countingVowel:boolean){
+    this.countingVowel = !countingVowel
+    this.handleChange();
+  }
+
+  isVowel(c:string):boolean {
+    return ['a', 'e', 'i', 'o', 'u'].indexOf(c.toLowerCase()) !== -1
+  }
+
+  isAlpha = function(ch:string){
+    return /^[A-Z]$/i.test(ch);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule,
+    BrowserAnimationsModule,
+    InputsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
